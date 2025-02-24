e27ddab87dc59dbeb3b45ee08575663de2207629
Leonardo <leondani853@gmail.com>
2025-02-24 10:15:15 -0300

    create user

--- src/main/java/academy/devdojo/UserServiceApplication.java
+++ src/main/java/academy/devdojo/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package academy_devdojo.user_service;
+package academy.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
