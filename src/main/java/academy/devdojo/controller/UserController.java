022ec23686bb4eba217a39c90c9d923735adc0c0
Leonardo <leondani853@gmail.com>
2025-02-24 10:20:13 -0300

    create user request and response

---
+++ src/main/java/academy/devdojo/controller/UserController.java
@@ -0,0 +1,14 @@
+package academy.devdojo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+}
