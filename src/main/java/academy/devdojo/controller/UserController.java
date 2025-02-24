1adb848e8fff94dc4130dc0784fded8d440da703
Leonardo <leondani853@gmail.com>
2025-02-24 10:22:15 -0300

    create user controller

--- src/main/java/academy/devdojo/controller/UserController.java
+++ src/main/java/academy/devdojo/controller/UserController.java
@@ -1,14 +1,32 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.response.UserGetResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/users")
 @Slf4j
 @RequiredArgsConstructor
 public class UserController {
+    @GetMapping
+    public ResponseEntity<List<UserGetResponse>> findAll(@RequestParam(required = false) String firstName){
+        log.debug("Request received to a list all users, param first name '{}'", firstName);
+
+        var users = service.findAll(firstName);
+
+        var userGetResponse = mapper.toUserGetResponseList(users);
+
+        return ResponseEntity.ok(userGetResponse);
+
+
+    }
 }
