c7e09b506ffaab7066d9cb9c75499133e1aebfe8
Leonardo <leondani853@gmail.com>
2025-03-05 15:21:34 -0300

    feat: validate if email is not used when saving or updating an user

--- src/main/java/academy/devdojo/repository/UserRepository.java
+++ src/main/java/academy/devdojo/repository/UserRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByFirstNameIgnoreCase(String firstName);
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndIdNot(String email, Long id);
 
 }
--- src/main/java/academy/devdojo/service/UserService.java
+++ src/main/java/academy/devdojo/service/UserService.java
@@ -4,7 +4,9 @@ import academy.devdojo.domain.User;
 import academy.devdojo.exception.NotFoundException;
 import academy.devdojo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class UserService {
     }
 
     public User save(User user) {
+        assertEmailDoesNoExist(user.getEmail());
         return repository.save(user);
     }
 
@@ -34,11 +37,26 @@ public class UserService {
 
     public void update(User userToUpdate) {
         assertUserExists(userToUpdate.getId());
+        assertEmailDoesNoExist(userToUpdate.getEmail(), userToUpdate.getId());
         repository.save(userToUpdate);
     }
 
     public void assertUserExists(Long id) {
         findByIdOrThrowNotFound(id);
+    }
+
+    public void assertEmailDoesNoExist(String email) {
+        repository.findByEmail(email)
+                .ifPresent(this::throwEmailExistsException);
+    }
+
+    public void assertEmailDoesNoExist(String email, Long id) {
+        repository.findByEmailAndIdNot(email, id)
+                .ifPresent(this::throwEmailExistsException);
+    }
+
+    private void throwEmailExistsException(User user) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "E-mail %s already exists".formatted(user.getEmail()));
     }
 
 
