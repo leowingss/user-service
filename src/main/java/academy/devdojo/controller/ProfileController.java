39bc7421c6f3fdc5b198f1f5f9e52bb07de771d4
Leonardo <leondani853@gmail.com>
2025-03-10 12:20:40 -0300

    feat: add Profile controller

---
+++ src/main/java/academy/devdojo/controller/ProfileController.java
@@ -0,0 +1,64 @@
+package academy.devdojo.controller;
+
+import academy.devdojo.mapper.ProfileMapper;
+import academy.devdojo.request.ProfilePostRequest;
+import academy.devdojo.response.ProfileGetResponse;
+import academy.devdojo.response.ProfilePostResponse;
+import academy.devdojo.service.ProfileService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/profiles")
+@Slf4j
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService service;
+    private final ProfileMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<ProfileGetResponse>> findAll() {
+        log.debug("Request received to a list all profiles");
+
+        var profiles = service.findAll();
+
+        var profileGetResponse = mapper.toProfileGetResponseList(profiles);
+
+        return ResponseEntity.ok(profileGetResponse);
+
+    }
+
+    @GetMapping("/paginated")
+    public ResponseEntity<Page<ProfileGetResponse>> findAllPaginated(Pageable pageable) {
+        log.debug("Request received to a list all profiles paginated");
+
+        var pageProfileGetResponse = service.findAllPaginated(pageable).map(mapper::toProfileGetResponse);
+        return ResponseEntity.ok(pageProfileGetResponse);
+
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<ProfilePostResponse> save(@RequestBody @Valid ProfilePostRequest profilePostRequest) {
+
+        log.debug("Request to save profile: {}", profilePostRequest);
+
+        var profile = mapper.toProfile(profilePostRequest);
+        var profileSaved = service.save(profile);
+        var profilePostResponse = mapper.toProfilePostResponse(profileSaved);
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(profilePostResponse);
+
+    }
+
+}
