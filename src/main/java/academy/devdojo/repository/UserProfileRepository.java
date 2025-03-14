047e3f40052fec80f5d6ac2414af0805c6bff2bc
Leonardo <leondani853@gmail.com>
2025-03-14 12:52:27 -0300

    refactor: use EntityGraph to optimize UserProfile query

--- src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -1,10 +1,20 @@
 package academy.devdojo.repository;
 
 import academy.devdojo.domain.UserProfile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
+
+//    @EntityGraph(attributePaths = {"user", "profile"})
+    @EntityGraph(value = "UserProfile.fullDetails")
+    List<UserProfile> findAll();
 }
