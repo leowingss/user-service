7db4b3cadf3e25c37ad9c50dedf5435ebf46c6b9
Leonardo <leondani853@gmail.com>
2025-03-24 16:46:07 -0300

    test: add integration test using RestAssured

--- src/test/java/academy/devdojo/commons/FileUtils.java
+++ src/test/java/academy/devdojo/commons/FileUtils.java
@@ -1,5 +1,6 @@
 package academy.devdojo.commons;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,8 @@ public class FileUtils {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    public String readResourceFile(String fileName) throws IOException {
+    @SneakyThrows
+    public String readResourceFile(String fileName){
         var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
         return new String(Files.readAllBytes(file.toPath()));
     }
