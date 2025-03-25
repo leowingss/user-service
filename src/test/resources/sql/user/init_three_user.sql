bec755f491b9bd92929f16251abf45c17e298da6
Leonardo <leondani853@gmail.com>
2025-03-25 16:55:18 -0300

    test: ignore dynamic ids in user integration test

---
+++ src/test/resources/sql/user/init_three_user.sql
@@ -0,0 +1,3 @@
+insert into user (email,first_name,last_name) values ('fulano@email.com','Fulano','Santos')
+insert into user (email,first_name,last_name) values ('ciclano@email.com','Ciclano','Silva')
+insert into user (email,first_name,last_name) values ('beltrano@email.com','Beltrano','Almeida')
