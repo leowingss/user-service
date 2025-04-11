8940390d86551f1675a088bc05aab209b46def1c
Leonardo <leondani853@gmail.com>
2025-04-11 11:23:54 -0300

    feat: configure Flyway for integration test database

--- src/test/java/academy/devdojo/config/TestcontainersConfiguration.java
+++ src/test/java/academy/devdojo/config/TestcontainersConfiguration.java
@@ -15,7 +15,8 @@ public class TestcontainersConfiguration {
     @ServiceConnection
     @RestartScope
     MySQLContainer<?> mysqlContainer() {
-        return new MySQLContainer<>(DockerImageName.parse("mysql:9.0.1"));
+        return new MySQLContainer<>(DockerImageName.parse("mysql:9.0.1"))
+            .withDatabaseName("user_service");
     }
 
 }
